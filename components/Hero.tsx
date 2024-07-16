5f0cb3ace3c113dd98224e9e9b278469b184abbf
Zakari Adamu <ojonimizakari@gmail.com>
2024-07-16 07:40:30 +0100

    feat: add a beautiful Hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
 	return (
@@ -21,9 +22,18 @@ const Hero = () => {
 				<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 			</div>
 			<div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
-        </div>
+				<div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+					<h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+						Dynamic Web Magic with Next.js
+					</h2>
+					<TextGenerateEffect
+						className="text-center text-[40px]	md:text-5xl lg:text-6xl"
+						words="Transforming Concepts into Seamless Experiences"
+					/>
+					<p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl ">
+						Hi I'm Zakari, a Next.js Developer based in Abuja.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
