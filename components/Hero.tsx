96b6cea7c651f5ea881cfe9fae12569a2b5b1cfd
Zakari Adamu <ojonimizakari@gmail.com>
2024-07-16 10:02:00 +0100

    feat: add CTA button to view my works

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
 	return (
@@ -28,11 +30,17 @@ const Hero = () => {
 					</h2>
 					<TextGenerateEffect
 						className="text-center text-[40px]	md:text-5xl lg:text-6xl"
-						words="Transforming Concepts into Seamless Experiences"
+						words="Transforming Concepts into Seamless User Experiences"
 					/>
 					<p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl ">
 						Hi I'm Zakari, a Next.js Developer based in Abuja.
 					</p>
+					<a href="#about">
+						<MagicButton title="Show My Work"
+							icon={<FaLocationArrow />}
+							position="right"
+						/>
+					</a>
 				</div>
 			</div>
 		</div>
